565c055b49521cf3d9ceb447807f3b72514da788
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-18 09:17:36 +0700

    add x

--- vd.cpp
+++ vd.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n,a[200001];
+void solve(){
+	cin>>n;
+	for(int i=0;i<n;i++) cin>>a[i];
+	
+}// neu ma code trong nay co ve cung co the duoc 
+// da tung co nhung luc code rat nhieu nen cai nay thi co ve la don gian:
 
 int main(){
 	cout << "hello world"<<endl;
