2e8577be94d65588583340074d88e5b88e22d6f9
Pham Minh Khoa <khoa.pham12397@hcmut.edu.vn>
2022-10-18 09:21:37 +0700

    add typedef

--- vd.cpp
+++ vd.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef long long ll;
 
+int n;
+
+void solve(){
+		
+}
 int main(){
 	cout << "hello world"<<endl;
 	cout <<"something"<<endl;
